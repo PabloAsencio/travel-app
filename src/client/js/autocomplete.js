88c3f2aa717f74d97e88dcbbe2708d1a7f3d008a
Pablo Asencio <8017043+PabloAsencio@users.noreply.github.com>
2020-12-17 11:59:09 +0100

    feat: Set up keyboard navigation for dropdown list
    The dropdown list now responds to keyboard events, so that it behaves in the expected way.
    - Created the functions navigateList, addActive and removeActive in autocomplete.js

--- src/client/js/autocomplete.js
+++ src/client/js/autocomplete.js
@@ -2,20 +2,27 @@
 function autocomplete(inputElement) {
     inputElement.addEventListener('input', handleInput);
     inputElement.addEventListener('keydown', navigateList);
+    document.addEventListener('click', clearCityList);
+
+    let currentFocus;
 
     async function handleInput() {
-        const value = this.value.trim();
+        const value = inputElement.value.trim();
         const country = document.getElementById('country');
         const latitude = document.getElementById('latitude');
         const longitude = document.getElementById('longitude');
 
-        /* If the value of the city field changes, we cannot be sure that the values of the country, latitude and longitude fields still match the user choice. We'd rather have no data than bad data.
+        /* If the value of the city field changes, we cannot be
+        sure that the values of the country, latitude and
+        longitude fields still match the user choice. We'd
+        rather have no data than bad data.
          */
         country.value = '';
         latitude.value = '';
         longitude.value = '';
         clearCityList();
         if (value) {
+            currentFocus = -1;
             const cities = await fetchCities(value);
             if (cities.length > 0) {
                 const listFragment = document.createDocumentFragment();
@@ -57,15 +64,69 @@ function autocomplete(inputElement) {
 
                     cityList.appendChild(listItem);
                 }
-                /* This second call is necessary: if the user types fast, the call to clearCityList above will happen before the list for the previous input has had time to be fetched and rendered. For the dropdown functionality to work as expected we have to make sure that any time we render a new list all previous ones are gone.*/
+                /* This second call is necessary: if the user
+                types fast, the call to clearCityList above will
+                happen before the list for the previous input
+                has had time to be fetched and rendered. For the
+                dropdown functionality to work as expected we
+                have to make sure that any time we render a new
+                list all previous ones are gone.*/
                 clearCityList();
-                this.parentNode.appendChild(listFragment);
+                inputElement.parentNode.appendChild(listFragment);
             }
         }
     }
-}
 
-function navigateList() {}
+    function navigateList(event) {
+        console.log('Keydown event triggered!');
+        const autocompleteList = document.getElementById(
+            'city-autocomplete-list'
+        );
+        if (autocompleteList) {
+            const cities = autocompleteList.getElementsByTagName('div');
+            switch (event.key) {
+                case 'Down':
+                case 'ArrowDown':
+                    currentFocus++;
+                    addActive(cities);
+                    break;
+                case 'Up':
+                case 'ArrowUp':
+                    if (currentFocus > -1) currentFocus--;
+                    addActive(cities);
+                    break;
+                case 'Enter':
+                    event.preventDefault();
+                    if (currentFocus > -1 && cities.length > currentFocus) {
+                        cities[currentFocus].click();
+                    } else {
+                        document.getElementById('tripInfo').submit();
+                    }
+                    break;
+                case 'Escape':
+                    clearCityList();
+                    break;
+            }
+        } else {
+            if (event.key == 'ArrowDown' || event.key == 'Down') {
+                handleInput();
+            }
+        }
+    }
+
+    function addActive(cities) {
+        if (cities) {
+            removeActive(cities);
+            if (currentFocus >= cities.length) {
+                currentFocus = 0;
+            } else if (currentFocus < 0) {
+                currentFocus = cities.length - 1;
+            }
+
+            cities[currentFocus].classList.add('autocomplete-active');
+        }
+    }
+}
 
 function clearCityList() {
     const autocompleteLists = document.getElementsByClassName(
@@ -73,6 +134,12 @@ function clearCityList() {
     );
     for (const list of autocompleteLists) {
         list.parentNode.removeChild(list);
+    }
+}
+
+function removeActive(cities) {
+    for (const city of cities) {
+        city.classList.remove('autocomplete-active');
     }
 }
 
