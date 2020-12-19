cf84d15355e899da4f2813c07f57f90cf9a73b6a
Pablo Asencio <8017043+PabloAsencio@users.noreply.github.com>
2020-12-19 15:24:07 +0100

    feat: Create call to /weather endpoint as part of handleSubmit

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,6 +1,24 @@
 function handleSubmit(event) {
     event.preventDefault();
-    console.log('Handling form submission!');
+    // TODO: Remove references to localhost from final version
+    let url = 'http://localhost:8082/weather?';
+    const latitude = document.getElementById('latitude').value;
+    const longitude = document.getElementById('longitude').value;
+
+    if (latitude & longitude) {
+        url += `latitude=${latitude}&longitude=${longitude}`;
+    } else {
+        const [city, province, country] = document
+            .getElementById('city')
+            .value.split[','].map((word) => word.trim());
+        url += `city=${city || ''}&province=${province || ''}&country=${
+            country || ''
+        }`;
+    }
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((weather) => console.log(weather));
 }
 
 export { handleSubmit };
