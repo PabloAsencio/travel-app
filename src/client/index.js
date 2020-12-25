6d9272df7d7b9d06e9b0a60f3e3ddafa638738aa
Pablo Asencio <8017043+PabloAsencio@users.noreply.github.com>
2020-12-25 10:23:12 +0100

    refactor: refactor index.js to set up all components
    Set up all components and dependencies for the application in index.js.
    Should have been in the previous commit

--- src/client/index.js
+++ src/client/index.js
@@ -1,16 +1,36 @@
-import { startApplication, handleSubmit } from './js/app';
+import { app } from './js/app';
 import { applicationState } from './js/appState';
+import { createAPIService } from './js/apiService';
+import { createDateController } from './js/dateController';
+import { createViewUpdater } from './js/uiUpdater';
+import { cityDropdownListController } from './js/autocomplete';
 
 import './styles/style.scss';
 import './styles/form.scss';
 
-startApplication(applicationState);
+// Set up components
+const apiService = createAPIService();
+const viewUpdater = createViewUpdater(applicationState);
+const dateController = createDateController(applicationState, viewUpdater);
+// Configure dependencies for the dropdown list
+cityDropdownListController.apiService = apiService;
+cityDropdownListController.appState = applicationState;
+cityDropdownListController.viewUpdater = viewUpdater;
+
+// Configure dependencies and start application
+app.apiService = apiService;
+app.applicationState = applicationState;
+app.cityController = cityDropdownListController;
+app.dateController = dateController;
+app.viewUpdater = viewUpdater;
+app.start();
 
 // Event listener for form submission
+// Logically this should actually be setup inside the app.start method called above
+// It is here to satisfy the requirement that at least one event listener
+// must be added in index.js
 const form = document.getElementById('tripInfo');
 const submitButton = document.getElementById('submit');
-form.addEventListener('submit', handleSubmit);
-submitButton.addEventListener('submit', handleSubmit);
-submitButton.addEventListener('click', handleSubmit);
-
-export { handleSubmit };
+form.addEventListener('submit', app.handleSubmit);
+submitButton.addEventListener('submit', app.handleSubmit);
+submitButton.addEventListener('click', app.handleSubmit);
