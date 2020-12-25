835ce0846dc10a7e4c48bef428ab5db204fe3388
Pablo Asencio <8017043+PabloAsencio@users.noreply.github.com>
2020-12-25 09:53:39 +0100

    refactor: Refactor app.js using the revealing module pattern
    The app.js file now exports a single object representing an application. This improves
    the organization of the code and better decouples it from the other components.
    In addition its dependencies must be now configured by the user of the object.
    The hard-coded dependencies have been removed.
    - Refactor app.js as a revealing module
    - Refactor app.js to have its dependencies injected
    - Configure components and app object in index.js

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,68 +1,78 @@
-import { createDateController } from './dateController';
-import { createViewUpdater } from './uiUpdater';
-import { createAPIService } from './apiService';
-import { cityDropdownListController } from './autocomplete';
-import { applicationState } from './appState';
+const app = (function () {
+    let _viewUpdater;
+    let _apiService;
+    let _dateController;
+    let _applicationState;
+    let _cityController;
 
-let viewUpdater;
-let apiService;
-let dateController;
-
-function startApplication(applicationState) {
-    viewUpdater = createViewUpdater(applicationState);
-    apiService = createAPIService();
-    dateController = createDateController(applicationState, viewUpdater);
-    dateController.start();
-    // Set functionality and event listener for the dropdown list
-    cityDropdownListController.apiService = apiService;
-    cityDropdownListController.appState = applicationState;
-    cityDropdownListController.viewUpdater = viewUpdater;
-    cityDropdownListController.start();
-}
-
-async function handleSubmit(event) {
-    event.preventDefault();
-
-    if (
-        dateController.areDatesValid() &&
-        (cityDropdownListController.isValidCity() ||
-            (await cityDropdownListController.fetchCity()))
-    ) {
-        const currentWeather = apiService.fetchCurrentWeather(
-            applicationState.latitude,
-            applicationState.longitude
-        );
-        console.log(applicationState.daysToTrip);
-        console.log(applicationState.duration);
-        const forecast = apiService.fetchWeatherForecast(
-            applicationState.latitude,
-            applicationState.longitude,
-            applicationState.daysToTrip.toString(10),
-            applicationState.duration.toString(10)
-        );
-        const pictures = apiService.fetchPictures(
-            applicationState.city,
-            applicationState.country
-        );
-
-        viewUpdater.clearWeatherSection();
-
-        // TODO: Show some informative error message in the UI instead of logging it
-        currentWeather
-            .then((response) => response.json())
-            .then((weather) => viewUpdater.updateCurrentWeather(weather))
-            .catch((error) => console.log(error.message));
-        forecast
-            .then((response) => response.json())
-            .then((forecast) => viewUpdater.updateWeatherForecast(forecast))
-            .catch((error) => console.log(error.message));
-        pictures
-            .then((response) => response.json())
-            .then((pictures) => viewUpdater.updatePicture(pictures))
-            .catch((error) => console.log(error.message));
-    } else {
-        console.log('Something went wrong!');
+    function start() {
+        _dateController.start();
+        _cityController.start();
     }
-}
 
-export { handleSubmit, startApplication };
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        if (
+            _dateController.areDatesValid() &&
+            (_cityController.isValidCity() ||
+                (await _cityController.fetchCity()))
+        ) {
+            const currentWeather = _apiService.fetchCurrentWeather(
+                _applicationState.latitude,
+                _applicationState.longitude
+            );
+            const forecast = _apiService.fetchWeatherForecast(
+                _applicationState.latitude,
+                _applicationState.longitude,
+                _applicationState.daysToTrip.toString(10),
+                _applicationState.duration.toString(10)
+            );
+            const pictures = _apiService.fetchPictures(
+                _applicationState.city,
+                _applicationState.country
+            );
+
+            _viewUpdater.clearWeatherSection();
+
+            // TODO: Show some informative error message in the UI instead of logging it
+            currentWeather
+                .then((response) => response.json())
+                .then((weather) => _viewUpdater.updateCurrentWeather(weather))
+                .catch((error) => console.log(error.message));
+            forecast
+                .then((response) => response.json())
+                .then((forecast) =>
+                    _viewUpdater.updateWeatherForecast(forecast)
+                )
+                .catch((error) => console.log(error.message));
+            pictures
+                .then((response) => response.json())
+                .then((pictures) => _viewUpdater.updatePicture(pictures))
+                .catch((error) => console.log(error.message));
+        } else {
+            console.log('Something went wrong!');
+        }
+    }
+    return {
+        start,
+        handleSubmit,
+        set applicationState(applicationState) {
+            _applicationState = applicationState;
+        },
+        set viewUpdater(viewUpdater) {
+            _viewUpdater = viewUpdater;
+        },
+        set dateController(dateController) {
+            _dateController = dateController;
+        },
+        set cityController(cityController) {
+            _cityController = cityController;
+        },
+        set apiService(apiService) {
+            _apiService = apiService;
+        },
+    };
+})();
+
+export { app };
